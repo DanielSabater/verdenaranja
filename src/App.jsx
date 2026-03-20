7774edfbc75ad0d2917c8d51a1d977f2a69f336d
Daniel Sabater <daniel038928@gmail.com>
2026-03-20 00:06:08 -0300

    reversion

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import ConfigView from "./components/views/ConfigView.jsx"
 import ClientesView from "./components/views/ClientesView.jsx"
 import Login from "./components/Login.jsx"
 
-const CELL_H = 110
+const CELL_H = 50
 
 export default function App() {
   const isMobile = useIsMobile()
